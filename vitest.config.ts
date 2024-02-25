ee8f2a2f23fb20c4fe1b50d258df45c1a880e30a
Cynthia Teeters <cynthiateeters@gmail.com>
2024-02-25 15:59:40 -0500

    Update packages

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,5 +4,9 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   test: {
     include: ['tests/*.test.js'],
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'json', 'html'],
+    },
   },
 })
