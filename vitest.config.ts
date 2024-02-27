1b41b5d115f0895c8d9657aa1b1b5d719f5877bf
Cynthia Teeters <cynthiateeters@gmail.com>
2024-02-27 13:17:05 -0500

    Update vitest.config.ts make parallel false

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,8 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   test: {
-    include: ['tests/*.test.js'],
+    fileParallelism: false,
+    include: ['./tests/*.test.js'],
     coverage: {
       provider: 'v8',
       reporter: ['text', 'json', 'html'],
